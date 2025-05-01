b90b7bf0cc56a3735135240e79eeec9513251450
Dimash <dingaidarbek@gmail.com>
2025-05-01 23:26:27 +0400

    first push

---
+++ src/main/java/com/example/news_website/services/MainService.java
@@ -0,0 +1,8 @@
+package com.example.news_website.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+    
+}
---
+++ src/test/java/com/example/news_website/NewsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

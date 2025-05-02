6a54a4a9b4be7603b0c37672b56cc8ba34e634fa
Dimash <dingaidarbek@gmail.com>
2025-05-02 23:17:18 +0400

    added categories and first ui

--- src/main/java/com/example/news_website/ApiController.java
+++ src/main/java/com/example/news_website/ApiController.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +20,12 @@ public class ApiController {
     public ResponseEntity<String> getNews() {
         String json = mainService.getNews(apiKey);
 
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(json);
+    }
+
+    @GetMapping("/news/categories")
+    public ResponseEntity<String> getNewsByCategory(@RequestParam String categories) {
+        String json = mainService.getNewsByCategories(apiKey, categories);
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(json);
     }
 }
--- src/main/java/com/example/news_website/MainService.java
+++ src/main/java/com/example/news_website/MainService.java
@@ -1,5 +1,6 @@
 package com.example.news_website;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,8 +12,15 @@ public class MainService {
         this.restTemplate = restTemplate;
     }
 
+    @Cacheable("news")
     public String getNews(String apiKey) {
         String url = "http://api.mediastack.com/v1/news?access_key="+apiKey+"&languages=en";
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    @Cacheable("categories")
+    public String getNewsByCategories(String apiKey, String categories) {
+        String url = "http://api.mediastack.com/v1/news?access_key="+apiKey+"&languages=en&categories="+categories;
         return restTemplate.getForObject(url, String.class);
     }
 }
